d2c44ac61bc60f1af824f7ae954ee9eebf7a2aa6
RenjithKI <renjith.kachappilly@gmail.com>
2018-02-25 15:09:22 +0000

    update 4

--- src/Adapter/VlcPlayer.java
+++ src/Adapter/VlcPlayer.java
@@ -1,12 +1,13 @@
 package Adapter;
-public class VlcPlayer implements AdvancedMediaPlayer{
-   @Override
-   public void playVlc(String fileName) {
-      System.out.println("Playing vlc file. Name: "+ fileName);		
-   }
 
-   @Override
-   public void playMp4(String fileName) {
-      //do nothing
-   }
-}
+public class VlcPlayer implements AdvancedMediaPlayer{
+	   @Override
+	   public void playVlc(String fileName) {
+	      System.out.println("Playing vlc file. Name: "+ fileName);		
+	   }
+
+	   @Override
+	   public void playMp4(String fileName) {
+	      //do nothing
+	   }
+	}
---
+++ src/Adapter2/MediaPlayerI.java
@@ -0,0 +1,4 @@
+package Adapter2;
+public interface MediaPlayerI {
+   public void play(String audioType, String fileName);
+}
