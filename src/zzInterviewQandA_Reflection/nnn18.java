c37694717dd7e08651c88143b78fb177803b2d08
RenjithKI <renjith.kachappilly@gmail.com>
2018-04-09 01:36:07 +0100

    update 8

--- src/zzInterviewQandA_Reflection/nnn18.java
+++ src/zzInterviewQandA_Reflection/nnn18.java
@@ -1,10 +1,10 @@
-package zzInterviewQandAHashMap;
+package zzInterviewQandA_Reflection;
 
 /**
  * @author Renjith
  *
  */
-public class nnn8 {
+public class nnn18 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
