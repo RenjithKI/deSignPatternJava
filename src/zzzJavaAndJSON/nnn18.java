c808658f4f76cff3bb80ab904b8afddb49c2db3d
RenjithKI <renjith.kachappilly@gmail.com>
2018-04-22 03:41:15 +0100

    update 9

--- src/zzzJavaAndJSON/nnn18.java
+++ src/zzzJavaAndJSON/nnn18.java
@@ -1,10 +1,10 @@
-package zzzOracleStream;
+package zzzJavaAndJSON;
 
 /**http://www.oracle.com/technetwork/articles/java/ma14-java-se-8-streams-2177646.html
  * @author Renjith
  *
  */
-public class nnn8 {
+public class nnn18 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/zzzJavaJSONJackson/nnn18.java
+++ src/zzzJavaJSONJackson/nnn18.java
@@ -1,10 +1,10 @@
-package zzzOracleStream;
+package zzzJavaJSONJackson;
 
 /**http://www.oracle.com/technetwork/articles/java/ma14-java-se-8-streams-2177646.html
  * @author Renjith
  *
  */
-public class nnn9 {
+public class nnn18 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
